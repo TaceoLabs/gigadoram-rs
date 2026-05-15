85940a505ee51389329ebabbddcab6aea98d36bb
Cesar199999 <cesar.descalzo2@gmail.com>
2026-05-15 12:55:15 +0200

    wip

--- crates/circuits/src/cht_lookup.rs
+++ crates/circuits/src/cht_lookup.rs
@@ -1,14 +1,15 @@
 use mpc_core::protocols::{
     rep3::Rep3State,
     rep3_ring::{
-        Rep3RingShare,
-        binary::{self, and_with_public, shift_r_public},
+        binary::{and_vec, and_with_public, shift_r_public},
         casts::downcast,
         ring::ring_impl::RingElement,
     },
 };
 use mpc_net::Network;
-use primitives::{BlockShare, XShare, bit_to_binary_mask, types::BitShare};
+use primitives::{
+    BlockShare, XShare, bit_to_binary_mask, is_zero_many, types::BitShare, upcast_x_to_block,
+};
 
 pub fn lookup_circuit(
     key: BlockShare,
@@ -27,26 +28,20 @@ pub fn lookup_circuit(
     let cht_b1_index = and_with_public(&cht_b1, &mask);
     let cht_b1_tag = shift_r_public(&cht_b1, shift);
 
-    // TODO: Single round
-    let key_equals_b0 = binary::is_zero(&(key_tag ^ cht_b0_tag), net, state)?;
-    let key_equals_b1 = binary::is_zero(&(key_tag ^ cht_b1_tag), net, state)?;
+    let equalities = is_zero_many(&[key_tag ^ cht_b0_tag, key_tag ^ cht_b1_tag], net, state)?;
+    let [key_equals_b0, key_equals_b1] = equalities.try_into().unwrap();
     let out_found = key_equals_b0 ^ key_equals_b1;
 
-    let key_equals_b0 = bit_to_binary_mask(&key_equals_b0);
-    let key_equals_b1 = bit_to_binary_mask(&key_equals_b1);
-    let dummy_index = upcast_binary_x_to_block(dummy_index);
+    let selection_masks = [
+        bit_to_binary_mask(&key_equals_b0),
+        bit_to_binary_mask(&key_equals_b1),
+    ];
+    let dummy_index = upcast_x_to_block(dummy_index);
+    let index_deltas = [cht_b0_index ^ dummy_index, cht_b1_index ^ dummy_index];
 
-    // TODO: Can we combine these?
-    let out_index = binary::cmux(&key_equals_b1, &cht_b1_index, &dummy_index, net, state)?;
-    let out_index = binary::cmux(&key_equals_b0, &cht_b0_index, &out_index, net, state)?;
+    let selected_deltas = and_vec(&selection_masks, &index_deltas, net, state)?;
+    let out_index = dummy_index ^ selected_deltas[0] ^ selected_deltas[1];
     let out_index = downcast(out_index);
 
     Ok((out_index, out_found))
-}
-
-fn upcast_binary_x_to_block(share: XShare) -> BlockShare {
-    Rep3RingShare::new_ring(
-        RingElement(u128::from(share.a.0)),
-        RingElement(u128::from(share.b.0)),
-    )
 }
