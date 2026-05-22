e55d3f977902315f37e3e671f8af6fab3b52340b
Cesar199999 <cesar.descalzo2@gmail.com>
2026-05-22 13:01:24 +0200

    [TAC-712] feat: add SpeedCache

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -5,4 +5,7 @@ pub use types::{
     BitShare, Block, BlockShare, X, XShare, Y, YShare, bit_to_binary_mask, from_2_shares,
     open_many, promote_public, promote_public_values, upcast_x_to_block, upcast_x_to_y,
 };
-pub use utils::{is_zero_many, low_u32, random_indexed_block, random_indexed_blocks, run_parties};
+pub use utils::{
+    cmux_many_custom, is_zero_many, low_u32, random_indexed_block, random_indexed_blocks,
+    run_parties,
+};
